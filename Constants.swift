86c098f0db03c29dd014a2d3ea726c5a2dc64d35
hardikaghera <hardikaghera2306@gmail.com>
2016-12-28 17:57:29 +0530

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  MsgApp
+//
+//  Created by hardik aghera on 24/12/16.
+//  Copyright © 2016 hardik aghera. All rights reserved.
+//
+
+struct Constants {
+    
+    // MARK: NotificationKeys
+    
+    struct NotificationKeys {
+        static let SignedIn = "onSignInCompleted"
+    }
+    
+    // MARK: MessageFields
+    
+    struct MessageFields {
+        static let name = "name"
+        static let text = "text"
+        static let imageUrl = "photoUrl"
+    }
+}
